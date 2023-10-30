8201cadc434594b2ab1bd7453646633577cf23ea
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-10-30 15:34:49 +0530

    profile image added

--- src/components/ui/shared/TopBar.tsx
+++ src/components/ui/shared/TopBar.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { useAuthContext } from "@/context/AuthContext";
 import { useSignOutAccount } from "@/lib/react-query/queryAndMutations";
 import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
@@ -6,6 +7,7 @@ import { Link, useNavigate } from "react-router-dom";
 export default function TopBar() {
   const { mutate: signOut, isSuccess } = useSignOutAccount();
   const navigate = useNavigate();
+  const { user } = useAuthContext();
 
   useEffect(() => {
     if (isSuccess) {
@@ -23,6 +25,13 @@ export default function TopBar() {
           <Button variant="ghost" className="shad-button_ghost" onClick={() => signOut()}>
             <img src="/assets/icons/logout.svg" alt="logout" />
           </Button>
+          <Link to={`/profile/${user.id}`} className="flex-center gap-3">
+            <img
+              alt="profile"
+              src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+              className="h-8 w-8 rounded-full"
+            />
+          </Link>
         </div>
       </div>
     </section>
