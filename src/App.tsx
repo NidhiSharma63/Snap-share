65286969183ceff281b1f194e93bcb51b65f4597
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-10-31 10:29:33 +0530

    path fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { AuthLayout, SignInForm, SignUpForm } from "@/_auth";
 import RootLayout from "@/_root/RootLayout";
 import { Home } from "@/_root/pages";
+import AllUsers from "@/_root/pages/AllUsers";
+import CreatePost from "@/_root/pages/CreatePost";
+import EditPost from "@/_root/pages/EditPost";
+import Explore from "@/_root/pages/Explore";
+import PostDetails from "@/_root/pages/PostDetails";
+import Profile from "@/_root/pages/Profile";
+import Saved from "@/_root/pages/Saved";
+import UpdateProfile from "@/_root/pages/UpdateProfile";
 import "@/styles/index.css";
 import { Route, Routes } from "react-router-dom";
-import AllUsers from "./_root/pages/AllUsers";
-import CreatePost from "./_root/pages/CreatePost";
-import EditPost from "./_root/pages/EditPost";
-import Explore from "./_root/pages/Explore";
-import PostDetails from "./_root/pages/PostDetails";
-import Profile from "./_root/pages/Profile";
-import Saved from "./_root/pages/Saved";
-import UpdateProfile from "./_root/pages/UpdateProfile";
 
 export default function App() {
   return (
