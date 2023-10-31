07687aa4d5be7888e9726f52bec600ad3643ec74
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-10-31 10:42:27 +0530

    bottom bar added

--- src/components/ui/shared/BottomBar.tsx
+++ src/components/ui/shared/BottomBar.tsx
@@ -1,3 +1,28 @@
+import { bottombarLinks } from "@/lib/constant/sidebar";
+import { Link, useLocation } from "react-router-dom";
+
 export default function BottomBar() {
-  return <div>BottomBar</div>;
+  const { pathname } = useLocation();
+  return (
+    <section className="bottom-bar">
+      {bottombarLinks.map((link) => {
+        const isActive = pathname === link.route;
+        return (
+          <Link
+            to={link.route}
+            key={link.label}
+            className={`${isActive && "bg-primary-500 rounded-[10px]"} flex-center flex-col gap-1 p-2 transition`}>
+            <img
+              width={16}
+              height={16}
+              src={link.imgURL}
+              alt={link.label}
+              className={`${isActive && "invert-white"}`}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
 }
