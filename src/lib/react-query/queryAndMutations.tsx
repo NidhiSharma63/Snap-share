4ffe763e14d2971cc6c072328e8af1722642725b
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-11-01 06:59:21 +0530

    all query key added

--- src/lib/react-query/queryAndMutations.tsx
+++ src/lib/react-query/queryAndMutations.tsx
@@ -1,6 +1,7 @@
 import { createPost, createUserAccount, signInAccount, signOutAccount } from "@/lib/appwrite/api";
 import { INewPost, INewUser } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { QUERY_KEYS } from "./querykeys";
 
 export const useCreateUserAccount = () => {
   return useMutation({
@@ -25,9 +26,10 @@ export const useCreatePost = () => {
   return useMutation({
     mutationFn: (post: INewPost) => createPost(post),
     onSuccess: () => {
-      // queryClient.invalidateQueries({
-      //   queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
-      // });
+      queryClient.invalidateQueries({
+        // queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
     },
   });
 };
