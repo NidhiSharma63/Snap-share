99f59498c85178ac37902b03f076b0a8a71e1ef4
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-11-01 14:54:11 +0530

    validation updated

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -24,7 +24,7 @@ export const signInFormSchema = z.object({
 
 export const postFormSchema = z.object({
   caption: z.string().min(5, { message: "Caption is too short" }).max(2200, { message: "Caption is too long" }),
-  tags: z.string().min(1, { message: "Password require" }),
+  tags: z.string(),
   file: z.custom<File[]>(),
-  location: z.string().min(2).max(100),
+  location: z.string().min(0).max(100),
 });
